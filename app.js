ef3231d55673331e9fe6a5aef58c079d95da3930
Shubhanshu Singh <shubhanshusingh602@gmail.com>
2025-12-09 23:55:37 +0530

    its day 2 of learning typescript

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 function shubh(obj) {
 }
 shubh({ name: "shubhanshu", email: "sadjhds@", password: 'dhhuhdfhd' });
 function yuv(obj) {
 }
+//FUNCTION IN TYPE-SCRIPT 
+function shubham(a) {
+    return a;
+}
+console.log(shubham('shubhnahsu'));
+//# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -17,3 +17,16 @@ shubh({name:"shubhanshu",email:"sadjhds@",password:'dhhuhdfhd'})
 function yuv(obj:admin){
 
 }
+//FUNCTION IN TYPE-SCRIPT 
+function shubham (a:string):String {
+return a;
+}
+
+console.log(shubham('shubhnahsu'));
+// CALL BACK FUNCTION DECLARATION
+function hellow (a:number,b:number,cbf:(c:number)=>void){
+console.log(a,b,cbf(44))
+}
+hellow(55,55,(c:number)=>{
+  console.log(55+6)
+})
